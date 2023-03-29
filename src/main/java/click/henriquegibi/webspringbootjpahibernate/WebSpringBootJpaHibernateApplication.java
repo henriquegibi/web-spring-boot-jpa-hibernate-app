e65fe4ff8a8fe2f910dc37c2780835bd80c43e31
HenriqueGibi <henriquedepadua@yahoo.com.br>
2023-03-29 19:36:02 -0300

    User entity and resource

---
+++ src/main/java/click/henriquegibi/webspringbootjpahibernate/WebSpringBootJpaHibernateApplication.java
@@ -0,0 +1,13 @@
+package click.henriquegibi.webspringbootjpahibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSpringBootJpaHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSpringBootJpaHibernateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/click/henriquegibi/webspringbootjpahibernate/WebSpringBootJpaHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package click.henriquegibi.webspringbootjpahibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSpringBootJpaHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
