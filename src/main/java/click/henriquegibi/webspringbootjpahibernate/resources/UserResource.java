15f67bccc85d972b71139db04144c5d45a2f3de6
HenriqueGibi <henriquedepadua@yahoo.com.br>
2023-04-26 08:30:05 -0300

    User delete

--- src/main/java/click/henriquegibi/webspringbootjpahibernate/resources/UserResource.java
+++ src/main/java/click/henriquegibi/webspringbootjpahibernate/resources/UserResource.java
@@ -43,4 +43,11 @@ public class UserResource {
                 .toUri();
         return ResponseEntity.created(uri).body(obj);
     }
+    
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id)
+    {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
