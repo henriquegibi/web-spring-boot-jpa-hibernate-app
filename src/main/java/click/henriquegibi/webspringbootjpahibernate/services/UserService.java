f62c9a51376e25e84393561d3163b16302fd65e3
HenriqueGibi <henriquedepadua@yahoo.com.br>
2023-04-26 15:54:22 -0300

    Exception handling - findById

--- src/main/java/click/henriquegibi/webspringbootjpahibernate/services/UserService.java
+++ src/main/java/click/henriquegibi/webspringbootjpahibernate/services/UserService.java
@@ -2,6 +2,7 @@ package click.henriquegibi.webspringbootjpahibernate.services;
 
 import click.henriquegibi.webspringbootjpahibernate.entities.User;
 import click.henriquegibi.webspringbootjpahibernate.repositories.UserRepository;
+import click.henriquegibi.webspringbootjpahibernate.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,7 +22,7 @@ public class UserService {
     public User findById(Long id)
     {
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
     
     public User insert(User obj)
